f0544bda434caf23b946ec2c4aa077c9c0f22c62
Emil van der Westhuizen <vdwemil@protonmail.com>
2023-07-13 12:30:40 +0200

    Fix import ordering

--- django_reports/models.py
+++ django_reports/models.py
@@ -2,8 +2,8 @@
 from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django_reports.filter import validate_filter_data
 
+from django_reports.filter import validate_filter_data
 from django_reports.validators import validate_model_label
 
 
